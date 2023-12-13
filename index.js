be5616ffc25d0fd320013a8f4daa860276abaf8a
João Victor Bomtempo Costa <bomtempojoaovictor2005@gmail.com>
2023-12-13 10:09:08 -0300

    confirm

--- index.js
+++ index.js
@@ -227,8 +227,8 @@ app.get('/', autentic, (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const usuario = req.body.userName_log;
-    const senha = req.body.userPass_log;
+    const usuario = req.body.USUARIO.userName;
+    const senha = req.body.USUARIO.Password;
     const usuarioCadastrado = user_list.find(user => user.userName === usuario && user.Password === senha);
     if (usuarioCadastrado) {
         req.session.usuAutenticar = true;
